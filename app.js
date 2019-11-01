2525cf1b79e803666a326610359f39573ec057bb
Diogo Soares Ferreira <diogodsdf@hotmail.com>
2019-11-01 19:01:00 -0300

    carregar aquivos html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Gerenciador Nutricional");
+  res.sendFile(__dirname + "/src/index.html");
 });
 
 app.get("/contato", function(req, res){
