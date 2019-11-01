e986e0e432ac6657b96df26d7148293b03449212
Diogo Soares Ferreira <diogodsdf@hotmail.com>
2019-11-01 18:09:45 -0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var http = require('http')
-const hostname = '127.0.0.1'
-const port = 3000
-const serv = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello, World!\n');
-})
+var http = require('http');
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`)
-}) 
+http.createServer(function(req, res){
+  res.end("Gerenciador Financeiro");
+}).listen(8080);
