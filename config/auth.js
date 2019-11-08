28b96338364ec828ced556a70fb95ebf43535708
Diogo Soares Ferreira <diogodsdf@hotmail.com>
2019-11-07 22:29:56 -0300

    add module passport

--- config/auth.js
+++ config/auth.js
@@ -6,28 +6,29 @@ const Usuario = mongoose.model('usuario')
 
 module.exports = function (passport) {
   passport.use(new localStrategy({ usernameField: 'email', passwordField: 'password' }, (email, password, done) => {
-    Usuario.findOne({ email: email }).then((usuario) => {
-      if (!usuario) {
-        return done(null, false, { message: "Cadastro com este e-mail não encontrado" })
-      }
-      
-      bcryptjs.compare(password, usuario.password, (erro, correta) => {
-        if (correta) {
-          return done(null, usuario)
-        } else {
-          return done(null, false, { message: "Dados de acesso incorretos!" })
-        }
-      })
-    })
-  }))
-}
+      Usuario.findOne({ email: email }).then((usuario) => {
+          if (!usuario) {
+              return done(null, false, { message: "Cadastro com este e-mail não encontrado!" })
+          }
 
-//Salvar os dados do usuário na sessão
-passport.serializaUser((usuario, done) => {
-  done(null, usuario.id)
-})
-passport.deserializeUser((id, done) => {
-  Usuario.findById(id, (erro, usuario) => {
-    done(erro, usuario)
+          bcryptjs.compare(password, usuario.password, (erro, correta) => {
+              if (correta) {
+                  return done(null, usuario)
+              } else {
+                  return done(null, false, { message: "Dados de acesso incorretos!" })
+              }
+          })
+      })
+  }))
+
+  //Salvar os dados do usuário na sessão
+  passport.serializeUser((usuario, done) => {
+      done(null, usuario.id)
   })
-})
+  passport.deserializeUser((id, done) => {
+      Usuario.findById(id, (erro, usuario) => {
+          done(erro, usuario)
+      })
+  })
+
+}
